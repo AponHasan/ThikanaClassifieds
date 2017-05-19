16eb8829edda51f01040e193318f8c59800572bc
AponHasan <mozammal.bubt89@gmail.com>
2017-05-20 02:41:36 +0600

    test1

--- ThikanaClassifieds/ThikanaClassifieds/Models/User.cs
+++ ThikanaClassifieds/ThikanaClassifieds/Models/User.cs
@@ -9,7 +9,7 @@ namespace ThikanaClassifieds.Models
     {
         public string Login { get; set; }
 
-        public string Name { get; set; }
+        public string UserName { get; set; }
 
         public string Password { get; set; }
     }
